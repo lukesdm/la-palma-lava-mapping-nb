7b39336aebccb384adaf1f77dc3a8d70656e3500
lukesdm <2824165+lukesdm@users.noreply.github.com>
2022-07-22 17:04:34 +0200

    wip

--- lplm_geoprocessing.py
+++ lplm_geoprocessing.py
@@ -1,9 +1,13 @@
 import geopandas
 import numpy
+import pandas
 import rasterio
 from shapely.geometry import shape
 from skimage.segmentation import slic
 import xarray
+
+## Segmentation
+## ------------
 
 def apply_segmentation(xds: xarray.Dataset,
                        date: numpy.datetime64,
@@ -52,3 +56,104 @@ def vectorize_segments(xds):
     gdf.sort_index(axis=0, inplace=True)
 
     return gdf
+
+
+## Lava analysis and region growing
+## --------------------------------
+## TODO: Below is failing or untested - fix it.
+
+def lava_mean_similarity(segment_data: geopandas.GeoSeries) -> float:
+    # Stats of stats - from empirical sampling of lava field segments from 2021-11-15 imagery, n=108.
+    MEAN_MEAN = 79.8
+    MEAN_STD = 13.1
+
+    mean_similarity = numpy.interp(
+        x = segment_data.post_mean,
+        xp = [MEAN_MEAN - 1.5 * MEAN_STD, MEAN_MEAN - 0.5 * MEAN_STD, MEAN_MEAN + 0.5 * MEAN_STD, MEAN_MEAN + 1.5 * MEAN_STD],
+        fp = [0.0, 1.0, 1.0, 0.0] )
+
+    return mean_similarity
+
+# Standard deviation (analog of texture) similarity.
+def lava_std_similarity(segment_data: geopandas.GeoSeries) -> float:
+    # Stats of stats - from empirical sampling of lava field segments from 2021-11-15 imagery, n=108.
+
+    std_similarity = numpy.interp(
+        x = segment_data.post_std,
+        xp = [18.0, 26.0, 38.0, 51.0],
+        fp = [0.0, 1.0, 1.0, 0.0] )
+
+    return std_similarity
+
+# Calculate lava-likeness index. Input should be a GeoSeries representing a single object
+def segment_lava_likeness(segment_data: geopandas.GeoSeries) -> float:
+    ll = (segment_data.lava_mean_similarity + segment_data.lava_std_similarity) / 2
+    return ll
+
+
+def get_neighbors(gdf, segment_id):
+    return gdf[gdf.touches(gdf.loc[segment_id].geometry)]
+
+
+def get_neighbors_of_neighbors(gdf, segment_id):
+    neighs = get_neighbors(gdf, segment_id)
+    n_of_n = []
+    for neighbor_segment_id, _ in neighs.iterrows():
+        n_of_n.append(get_neighbors(gdf, neighbor_segment_id))
+
+    to_exclude = [segment_id, *list(neighs.segment_id)]
+    n_of_n = pandas.concat(n_of_n).drop(to_exclude)
+
+    return geopandas.GeoDataFrame(n_of_n)
+
+
+def neighbourhood_lava_likeness(gdf, segment_id) -> float:
+    neighbors = get_neighbors(gdf, segment_id)
+    
+    # COULDDO: Consider neighbors of neighbors.
+    # neighbors_of_neighbors = get_neighbors_of_neighbors(gdf, segment_id)
+    
+    return (
+        0.5 * gdf.loc[segment_id]["segment_lava_likeness"] + 
+        0.5 * neighbors["neighbourhood_lava_likeness"].sum() / len(neighbors) )
+
+
+def get_unvisited_neighbors(gdf, segment_id, group_id):
+    all_neighbors = gdf[gdf.touches(gdf.loc[segment_id].geometry)]
+    new_neighbors = all_neighbors[gdf.group != group_id]
+    return new_neighbors
+
+
+# COULDDO: Parameterize
+LL_THRESHOLD = 0.5
+
+def lava_likeness_overall(gdf, start_segment_id):
+    gdf["neighbourhood_lava_likeness"] = gdf["segment_lava_likeness"]
+    gdf["group"] = gdf.segment_id
+    gdf.at[start_segment_id, "neighbourhood_lava_likeness"] = 1.0
+    to_visit = [start_segment_id]
+    while len(to_visit) > 0:
+        current_segment_id = to_visit.pop()
+        neighs = get_unvisited_neighbors(gdf, current_segment_id, start_segment_id)
+        for neigh_seg_id, _ in neighs.iterrows():
+            ll = neighbourhood_lava_likeness(gdf, neigh_seg_id)
+            if ll > LL_THRESHOLD:
+                gdf.at[neigh_seg_id, "neighbourhood_lava_likeness"] = 1.0
+                gdf.at[neigh_seg_id, "group"] = start_segment_id
+                to_visit.append(neigh_seg_id)
+
+    
+def extract_lava_region(gdf, start_segment_id):
+    return gdf[gdf["group"] == start_segment_id].dissolve()
+
+
+def enrich(xds, gdf, date):
+    # Calculate statistical features for segments. Available functions are listed here:
+    # https://xarray.pydata.org/en/v0.11.3/generated/xarray.core.groupby.DataArrayGroupBy.html
+    segments = xds.groupby('segment_id')
+    gdf["mean"] = segments.mean()
+    gdf["std"] = segments.std()
+    
+    gdf["lava_mean_similarity"] = gdf.apply(lava_mean_similarity, axis=1)
+    gdf["lava_std_similarity"] = gdf.apply(lava_std_similarity, axis=1)
+    gdf["segment_lava_likeness"] = gdf.apply(segment_lava_likeness, axis=1)
