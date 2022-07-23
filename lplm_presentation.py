80995a4836686f3340f81fb94f5ef128c7b6e5d1
lukesdm <2824165+lukesdm@users.noreply.github.com>
2022-07-23 16:08:48 +0200

    Map updates

--- lplm_presentation.py
+++ lplm_presentation.py
@@ -1,6 +1,8 @@
-import folium
+import ipyleaflet
 import geopandas
 import numpy
+
+import base64
 
 import lplm_io as lpio
 
@@ -9,17 +11,22 @@ from lplm_utils import format_date
 def get_date_labels(xds):
     return [(format_date(d), d) for d in xds.sortby("date").date.values]
 
-def build_map(gdf: geopandas.GeoDataFrame, date: numpy.datetime64) -> folium.Map:
-    """Builds a Folium map with segment layer and SAR image for the given date""" 
-    fol_map = gdf.explore(style_kwds={"fill": False, "weight": 1})
+def make_sar_layer(xds, date):
+    """
+    Creates a raster layer for the SAR data for the given date.
+
+    Note: Uses data-uri format so that no files have to be served.
+    This is ok for small images, like jpegs of our study area. 
+    """
+    bounds_4326 = xds.rio.transform_bounds("+init=epsg:4326")
+    bounds_leaf = (bounds_4326[1], bounds_4326[0]), (bounds_4326[3], bounds_4326[2])
+    # Use a .jpg version for presentation, due to the smaller file size.
     img_path = lpio.get_jpg(date)
-    bounds_gdf = gdf.to_crs("EPSG:4326").total_bounds
-    bounds_fol = [[bounds_gdf[3], bounds_gdf[2]], [bounds_gdf[1], bounds_gdf[0]]]
-    sar_overlay = folium.raster_layers.ImageOverlay(
-        name=f"SAR for {date}",
-        image=str(img_path),
-        z_index=1,
-        bounds=bounds_fol)
-    sar_overlay.add_to(fol_map)
-    folium.LayerControl().add_to(fol_map)
-    return fol_map
+    # data-uri encode
+    with open(img_path, 'rb') as img_file:
+        img_data = img_file.read()
+    data_url = 'data:image/jpeg;base64,' + base64.b64encode(img_data).decode('utf-8')
+    return ipyleaflet.ImageOverlay(
+        url=data_url,
+        bounds=bounds_leaf,
+        name=f"SAR (gamma0) @ {format_date(date)}")
